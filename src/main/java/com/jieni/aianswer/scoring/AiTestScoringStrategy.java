b0cd3a7a3e37b01ae31b7eeac32f0d47b61b6c48
jienisk <jienisk@outlook.com>
2025-06-27 02:15:15 +0800

    jj

--- src/main/java/com/jieni/aianswer/scoring/AiTestScoringStrategy.java
+++ src/main/java/com/jieni/aianswer/scoring/AiTestScoringStrategy.java
@@ -1,7 +1,11 @@
 package com.jieni.aianswer.scoring;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.jieni.aianswer.manager.AiManager;
 import com.jieni.aianswer.model.dto.question.QuestionAnswerDTO;
 import com.jieni.aianswer.model.dto.question.QuestionContentDTO;
@@ -12,12 +16,15 @@ import com.jieni.aianswer.model.entity.UserAnswer;
 import com.jieni.aianswer.model.vo.QuestionVO;
 import com.jieni.aianswer.service.QuestionService;
 import com.jieni.aianswer.service.ScoringResultService;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * AI测评类应用评分策略
@@ -33,6 +40,21 @@ public class AiTestScoringStrategy implements ScoringStrategy {
 
     @Resource
     private AiManager aiManager;
+
+//    @Resource
+//    private RedissonClient redissonClient;
+
+//    // 分布式锁的 key
+//    private static final String AI_ANSWER_LOCK = "AI_ANSWER_LOCK";
+
+    /**
+     * AI 评分结果本地缓存
+     */
+    private final Cache<String, String> answerCacheMap =
+            Caffeine.newBuilder().initialCapacity(1024)
+                    // 缓存 5 分钟移除
+                    .expireAfterAccess(5L, TimeUnit.MINUTES)
+                    .build();
 
     /**
      * AI 评分系统消息
@@ -55,6 +77,20 @@ public class AiTestScoringStrategy implements ScoringStrategy {
     @Override
     public UserAnswer doScore(List<String> choices, App app) throws Exception {
         Long appId = app.getId();
+        String jsonStr = JSONUtil.toJsonStr(choices);
+        String cacheKey = buildCacheKey(appId, jsonStr);
+        String answerJson = answerCacheMap.getIfPresent(cacheKey);
+        // 如果有缓存，直接返回
+        if (StrUtil.isNotBlank(answerJson)) {
+            // 构造返回值，填充答案对象的属性
+            UserAnswer userAnswer = JSONUtil.toBean(answerJson, UserAnswer.class);
+            userAnswer.setAppId(appId);
+            userAnswer.setAppType(app.getAppType());
+            userAnswer.setScoringStrategy(app.getScoringStrategy());
+            userAnswer.setChoices(jsonStr);
+            return userAnswer;
+        }
+
         // 1. 根据 id 查询到题目
         Question question = questionService.getOne(
                 Wrappers.lambdaQuery(Question.class).eq(Question::getAppId, appId)
@@ -72,13 +108,36 @@ public class AiTestScoringStrategy implements ScoringStrategy {
         int end = result.lastIndexOf("}");
         String json = result.substring(start, end + 1);
 
+        // 缓存结果
+        answerCacheMap.put(cacheKey, json);
+
         // 3. 构造返回值，填充答案对象的属性
         UserAnswer userAnswer = JSONUtil.toBean(json, UserAnswer.class);
         userAnswer.setAppId(appId);
         userAnswer.setAppType(app.getAppType());
         userAnswer.setScoringStrategy(app.getScoringStrategy());
-        userAnswer.setChoices(JSONUtil.toJsonStr(choices));
+        userAnswer.setChoices(jsonStr);
         return userAnswer;
+
+//        // 定义锁
+//        RLock lock = redissonClient.getLock(AI_ANSWER_LOCK + cacheKey);
+//        try {
+//            // 竞争锁
+//            boolean res = lock.tryLock(3, 15, TimeUnit.SECONDS);
+//            // 没抢到锁，强行返回
+//            if (!res) {
+//                return null;
+//            }
+//            // 抢到锁了，执行后续业务逻辑
+//
+//        } finally {
+//            if (lock != null && lock.isLocked()) {
+//                if (lock.isHeldByCurrentThread()) {
+//                    lock.unlock();
+//                }
+//            }
+//        }
+
     }
 
     /**
@@ -102,6 +161,18 @@ public class AiTestScoringStrategy implements ScoringStrategy {
         }
         userMessage.append(JSONUtil.toJsonStr(questionAnswerDTOList));
         return userMessage.toString();
+    }
+
+
+    /**
+     * 构建缓存 key
+     *
+     * @param appId
+     * @param choices
+     * @return
+     */
+    private String buildCacheKey(Long appId, String choices) {
+        return DigestUtil.md5Hex(appId + ":" + choices);
     }
 
 }
